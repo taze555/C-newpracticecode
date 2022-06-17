d94ceff02699a1e68a44cc98cd820de6fbb21116
taze555 <102717547+taze555@users.noreply.github.com>
2022-06-17 05:00:00 -0500

    Add files via upload

---
+++ A Very Big Sum/A Very Big Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace A_Very_Big_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static long aVeryBigSum(List<long> ar)
+        {
+            return ;
+        }
+    }
+}
