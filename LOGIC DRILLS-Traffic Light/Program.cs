54ba8a64f30314e3fef9d236f274249e34f15d74
Jacob Vondrasek <jacob.vondrasek@my.tccd.edu>
2022-06-17 04:56:51 -0500

    Add project files.

---
+++ LOGIC DRILLS-Traffic Light/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LOGIC_DRILLS_Traffic_Light
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        public static string UpdateLight(string current)
+        {
+            if (current.ToLower() == "green")
+            {
+                return "yellow";
+            }
+            else if (current.ToLower() == "yellow")
+            {
+                return "red";
+            }    
+            else
+            {
+                return "green";
+            }
+        }
+    }
+}
